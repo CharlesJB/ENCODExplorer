0159d0163e6b40cf9cd4c8517a407f6cdd8852dc
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2015-03-06 12:02:42 -0500

    Added the get_encode_types to generate encode type (table) names
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -1,30 +1,12 @@
 #' Create the RSQLite databse for the tables in ENCODE
 #'
-#' The following table are extracted by default:
-#' \describe{
-#'   \item{biosample}{}
-#'   \item{experiment}{}
-#'   \item{lab}{}
-#'   \item{dataset}{}
-#'   \item{files}{}
-#'   \item{target}{}
-#'   \item{organism}{}
-#'   \item{replicate}{}
-#'   \item{award}{}
-#'   \item{treatment}{}
-#'   \item{human_donor}{}
-#'   \item{library}{}
-#'   \item{platform}{}
-#'   \item{document}{}
-#' }
-#'
 #' @return a \code{list} with selected tables from ENCODE that were used to
 #' create the \code{RSQLite} database.
 #'
 #' @param database_filename The name of the file to save the database into.
 #' Default: \code{\"ENCODEdb.sqlite\"}.
 #' 
-#' @param type The tables to extract from ENCODE rest api.
+#' @param type The names of the tables to extract from ENCODE rest api.
 #' 
 #' @param overwrite Should tables already present in database be overwrited?
 #' Default: \code{FALSE}.
@@ -36,24 +18,83 @@
 #'  
 #'  @export
 prepare_ENCODEdb <- function(database_filename = "inst/extdata/ENCODEdb.sqlite",
-                             types = c("biosample","experiment", "lab",
-                                       "dataset", "file", "target", "organism",
-                                       "replicate", "award", "treatment",
-                                       "human_donor", "library", "platform",
-                                       "document"),
-                             overwrite = FALSE) {
+                             types = get_encode_types(), overwrite = FALSE) {
   # Extract the tables from the ENCODE rest api
   extract_type <- function(type) {
     table <- extract_table(type)
     table <- clean_table(table)
-    dbWriteTable(con, type, table, overwrite = overwrite)
+    if (all(dim(table) > 0)) {
+      RSQLite::dbWriteTable(con, type, table, overwrite = overwrite)
+    } else {
+      table <- NULL
+    }
     table
   }
-  con <- dbConnect(RSQLite::SQLite(), database_filename)  
+  con <- RSQLite::dbConnect(RSQLite::SQLite(), database_filename)
   tables <- lapply(types, extract_type)
-  dbDisconnect(con)
+  RSQLite::dbDisconnect(con)
   
   # Return the named tables
   names(tables) <- types
-  tables
+  tables[sapply(tables, is.null)] <- NULL
+}
+
+#' A list of known tables from ENCODE database.
+#'
+#' The list was extracted from:
+#'   https://github.com/ENCODE-DCC/encoded/tree/master/src/encoded/schemas
+#'
+#' @return a vector of \code{character} with the names of the known tables in
+#'   the ENCODE database.
+#'
+#' @examples
+#'   types <- get_encode_types()
+#'
+#'  @export
+# TODO: find a way to fetch this info from the github
+get_encode_types <- function() {
+  c("access_key",
+    "analysis_step",
+    "analysis_step_run",
+    "antibody_approval",
+    "antibody_characterization",
+    "antibody_lot",
+    "award",
+    "biosample",
+    "biosample_characterization",
+    "characterization",
+    "construct",
+    "construct_characterization",
+    "dataset",
+    "document",
+    "donor",
+    "donor_characterization",
+    "experiment",
+    "file",
+    "fly_donor",
+    "human_donor",
+    "image",
+    "lab",
+    "library",
+    "mixins",
+    "mouse_donor",
+    "namespaces",
+    "organism",
+    "page",
+    "pipeline",
+    "platform",
+    "publication",
+    "quality_metric",
+    "replicate",
+    "rnai",
+    "rnai_characterization",
+    "software",
+    "software_version",
+    "source",
+    "talen",
+    "target",
+    "treatment",
+    "user",
+    "workflow_run",
+    "worm_donor")
 }
