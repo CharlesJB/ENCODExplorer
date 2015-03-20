4228d377bc145136be3fd086fc5d3a59f9621131
nekomimi <nekomimi@daruma.(none)>
2015-03-20 16:56:37 -0400

    first push

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -1,7 +1,13 @@
-#' Create the RSQLite databse for the tables in ENCODE
+#' Create the RSQLite databse for the tables in ENCODE and extract basic 
+#' informations in a \code{list} of \code{data.frame}s
+#' 
 #'
-#' @return a \code{list} with selected tables from ENCODE that were used to
-#' create the \code{RSQLite} database.
+#' @return a \code{list} containing three elements. The first one 'tables' is a 
+#' \code{list} with selected tables from ENCODE that were used to
+#' create the \code{RSQLite} database ; the second one 'experiment' is a 
+#' \code{data.frame} containing basic informations for each file part of an 
+#' experiment ; the third one 'dataset' is a \code{data.frame} containing basic 
+#' informations for each file part of a dataset.
 #'
 #' @param database_filename The name of the file to save the database into.
 #' Default: \code{\"ENCODEdb.sqlite\"}.
@@ -19,25 +25,172 @@
 #'  @export
 prepare_ENCODEdb <- function(database_filename = "inst/extdata/ENCODEdb.sqlite",
                              types = get_encode_types(), overwrite = FALSE) {
-  # Extract the tables from the ENCODE rest api
-  extract_type <- function(type) {
-    table <- extract_table(type)
-    table <- clean_table(table)
-    if (all(dim(table) > 0)) {
-      RSQLite::dbWriteTable(con, type, table, overwrite = overwrite)
-    } else {
-      table <- NULL
-    }
-    table
+  
+  if(file.exists(database_filename)) {
+    warning(paste0("The file ", database_filename, " already exists. Please delete it before re-run the data preparation"))
   }
+  else
+  {
+    print(format(Sys.time(), "%a %b %d %X %Y"))
+    T1<-Sys.time()
+    # Extract the tables from the ENCODE rest api
+    extract_type <- function(type) {
+      
+      table <- extract_table(type)
+      table <- clean_table(table)
+      
+      if (all(dim(table) > 0)) {
+        RSQLite::dbWriteTable(con, type, table, overwrite = overwrite)
+      } else {
+        table <- NULL
+      }
+      table
+    }
+    con <- RSQLite::dbConnect(RSQLite::SQLite(), database_filename)
+    tables <- lapply(types, extract_type)
+    RSQLite::dbDisconnect(con)
+    
+    # Return the named tables
+    names(tables) <- types
+    tables[sapply(tables, is.null)] <- NULL
+    
+    Tdiff = Sys.time() - T1
+    print(paste0("Extract the tables from the ENCODE rest api : ",Tdiff, " min"))
+    
+    T2 = Sys.time()
+    # Extract data from the DB
+    if(length(tables) > 0) {
+      matrices = export_ENCODEdb_matrix(database_filename)
+      print(format(Sys.time(), "%a %b %d %X %Y"))
+      Tdiff = Sys.time() - T2
+      print(paste0("Extract data from the DB : ",Tdiff, " min"))
+      
+      invisible(list(tables = tables, experiment = matrices[[1]], 
+                     dataset = matrices[[2]]))
+    }
+    else
+    {
+      warning(paste0("Some goes wrong during the data preparation. 
+            Please erase the database ",database_filename," and re-run the whole process.
+            If the problem persists, please contact us"))
+      list()
+    }
+    
+  }
+}
+
+export_ENCODEdb_matrix <- function(database_filename) {
   con <- RSQLite::dbConnect(RSQLite::SQLite(), database_filename)
-  tables <- lapply(types, extract_type)
+  query_exp = "select e.accession as accession, target.name as target, e.possible_controls as controls, l.title as lab, e.date_released, e.status, e.assay_term_name as assay, e.biosample_type, e.biosample_term_name as biosample_name, e.assembly, e.run_type, f.accession as file_accession, f.file_format, f.paired_end, f.paired_with, f.platform, f.replicate as replicate_list, f.href FROM experiment as e, file as f, lab as l LEFT JOIN target ON e.target = target.id where f.dataset=e.id AND f.lab=l.id;"
+  query_ds = "select d.accession, l.title as lab, d.date_released, d.status as status, d.assembly, f.accession as file_accession, f.file_format, d.related_files, f.href from dataset as d, file as f, lab as l where f.dataset=d.id AND f.lab=l.id;"
+  
+  rs <- RSQLite::dbSendQuery(con, query_exp)
+  encode_exp <- RSQLite::dbFetch(rs, n = -1)
+  RSQLite::dbClearResult(rs)
+  
+  rs <- RSQLite::dbSendQuery(con, query_ds)
+  encode_ds <- RSQLite::dbFetch(rs, n = -1)
+  RSQLite::dbClearResult(rs)
+  
+  ############################ REQUETES SECONDAIRES ############################ 
+  
+  organism  = c()
+  treatment = c()
+  biological_replicate_number = c()
+  technical_replicate_number = c()
+  
+  for(replicate in encode_exp$replicate_list) {
+    if(!is.na(replicate)) {
+      
+      ### GET library to get ORGANISM and TREATMENT
+      query_lib = paste0("select library as lib from replicate where id=\"",replicate,"\"")
+      rs <- RSQLite::dbSendQuery(con, query_lib)
+      res <- RSQLite::dbFetch(rs, n = -1)
+      RSQLite::dbClearResult(rs)
+      
+      ## if there is no library, there is no way to get the organism neither the treatment infos 
+      if(length(res$lib) == 0) {
+        organism = c(organism,NA)
+        treatment = c(treatment,NA)
+      }
+      else
+      {
+        rep.library = res$lib
+        ### GET ORGANISM 
+        query_org = paste0("select o.scientific_name as organism from organism as o where id= (select organism from biosample where id=(select biosample from library where id=\"",rep.library,"\"));")
+        rs <- RSQLite::dbSendQuery(con, query_org)
+        res2 <- RSQLite::dbFetch(rs, n = -1)
+        RSQLite::dbClearResult(rs)
+        
+        if(length(res2$organism) == 0) {
+          organism = c(organism,NA)
+        }
+        else
+        {
+          organism = c(organism,res2$organism)
+        }
+        
+        ### GET TREATMENT 
+        query_treat = paste0("select treatment_term_name from treatment where id = (select treatments from library where id=\"",rep.library,"\");")
+        rs <- RSQLite::dbSendQuery(con, query_treat)
+        res2 <- RSQLite::dbFetch(rs, n = -1)
+        RSQLite::dbClearResult(rs)
+        
+        if(length(res2$treatment_term_name) == 0) {
+          treatment = c(treatment,NA)
+        }
+        else
+        {
+          treatment = c(treatment,res2$treatment_term_name)
+        }
+      }
+      ### GET REPLICATE INFOS 
+      query_rep = paste0("select technical_replicate_number,biological_replicate_number from replicate where id=\"",replicate,"\";")
+      rs <- RSQLite::dbSendQuery(con, query_rep)
+      res2 <- RSQLite::dbFetch(rs, n = -1)
+      RSQLite::dbClearResult(rs)
+      technical_replicate_number = c(technical_replicate_number,res2$technical_replicate_number)
+      biological_replicate_number = c(biological_replicate_number,res2$biological_replicate_number)
+      
+    } else {
+      
+      organism = c(organism,NA)
+      treatment = c(treatment,NA) 
+      technical_replicate_number = c(technical_replicate_number,NA)
+      biological_replicate_number = c(biological_replicate_number,NA)
+      
+    }
+  }
+  encode_exp = cbind(encode_exp, organism, treatment, technical_replicate_number, biological_replicate_number)
+  
   RSQLite::dbDisconnect(con)
   
-  # Return the named tables
-  names(tables) <- types
-  tables[sapply(tables, is.null)] <- NULL
+  
+  ############################ AJOUT LIENS AVEC DATASET ############################ 
+  
+  dataset_accession = rep(NA, nrow(encode_exp))
+  names(dataset_accession) = as.character(encode_exp$file_accession)
+  dataset_with_file = subset(encode_ds, ! is.na(encode_ds$related_files), select = c(accession, related_files))
+  dataset_with_file = unique(dataset_with_file)
+  
+  for (i in (1:nrow(dataset_with_file))) {
+    ds = dataset_with_file[i,]
+    dsId = as.character(ds$accession)
+    related_files = strsplit(ds$related_files, split = ";")[[1]]
+    for (f in related_files) {
+      f = strsplit(f, split = "/")[[1]][3]
+      if(f %in% names(dataset_accession)) {
+        dataset_accession[[f]] = dsId
+      }
+    }    
+  }
+  
+  encode_exp = cbind(encode_exp, dataset_accession)
+  
+  list(experiment = encode_exp, dataset = encode_ds)
 }
+
+
 
 #' A list of known tables from ENCODE database.
 #'
--- R/rest_api.R
+++ R/rest_api.R
@@ -9,8 +9,12 @@
 #' @examples
 #' lab <- ENCODEdb:::extract_table("lab")
 extract_table <- function(type) {
+  filters = "&limit=all"
+  filters = paste0(filters, "&frame=object")
+  
   url <- "https://www.encodeproject.org/search/?type="
-  url <- paste0(url, type, "&limit=all")
+  url <- paste0(url, type, filters)
+  
   res <- jsonlite::fromJSON(url)
   if (res[["notification"]] != "Success") {
     data.frame()
@@ -37,9 +41,9 @@ extract_table <- function(type) {
 #' lab <- ENCODEdb:::extract_table("lab")
 #' lab <- ENCODEdb:::clean_table(lab)
 clean_table <- function(table) {
+  
   clean_column <- function(column_name) {
     column <- table[[column_name]]
-
     # Case: data.frame
     if (is.data.frame(column)) {
       if (ncol(column) == 1 & nrow(column) == nrow(table)) {
@@ -47,15 +51,15 @@ clean_table <- function(table) {
       } else {
         column <- NULL
       }
-
-    # Case: list
+      
+      # Case: list
     } else if (is.list(column)) {
       # List of empty list
       if (all(sapply(column, length) == 0)) {
         column <- NULL
       } else if (column_name == "@type") {
         column <- NULL
-      # List of character vector
+        # List of character vector
       } else if (all(sapply(column, class) == "character")) {
         if (all(sapply(column, length) <= 1)) {
           column <- sapply(column, function(x) {
@@ -66,12 +70,20 @@ clean_table <- function(table) {
             }
           })
         } else {
-          column <- NULL
+          column <- sapply(column, function(x) {
+            if (length(x) > 0) {
+              paste(x, collapse = ";")
+            }
+            else {
+              NA
+            }
+          })
+          
         }
         # List of data.frames
       } else if (all(sapply(column, class) == "data.frame")) {
         if (all(sapply(column, nrow) <= 1) &
-            all(sapply(column, ncol) <= 1)) {
+              all(sapply(column, ncol) <= 1)) {
           column <- sapply(column, function(x) {
             if (length(x) > 0) {
               x[[1,1]]
@@ -82,8 +94,9 @@ clean_table <- function(table) {
         } else {
           column <- NULL
         }
+      }
       # List of something else
-      } else {
+      else {
         column <- NULL
       }
     }
@@ -94,4 +107,379 @@ clean_table <- function(table) {
   names(table) <- table_names
   table[sapply(table, is.null)] <- NULL
   as.data.frame(table)
+}
+
+#' Simulate a query on ENCODE website and return the result as a 
+#' \code{data.frame}
+#'
+#' \code{data.frame}s produced when converting JSON to \code{data.frame} with
+#' the \code{fromJSON} function will sometime have columns that are lists
+#' and/or columns that are \code{data.frames}.
+#'
+#' This function simulates a basic query on ENCODE website 
+#'
+#' @param searchTerm a search term
+#' @param limit the maximum number of return entries, default 10. \code{limit = all}
+#' will return all the result. It can generate large results set.
+#'
+#' @return a \code{data.frame} corresponding Every object that matches the 
+#' search term
+#'
+#' @examples
+#' res = ENCODEdb::search(searchTerm = "mcf7", limit = 2)
+search <- function(searchTerm, limit = "10") {
+  filters = paste0("searchTerm=",searchTerm, "&limit=", limit)
+  url <- "https://www.encodeproject.org/search/?"
+  url <- paste0(url, filters)
+  
+  res <- jsonlite::fromJSON(url)
+  if (res[["notification"]] != "Success") {
+    warning("No result found", call. = F)
+    r = data.frame()
+  } else {
+    r = res[["@graph"]]
+  }
+  
+  r = clean_table(r)
+  r
+}
+
+#' Produce a subset of data following predefined criteria
+#'
+#' After running the \code{prepare_ENCODEDb} function, this function will allow 
+#' you to extract a subset of data encording to the following criteria : 
+#' accession, assay name, biosample, dataset accession, file accession, 
+#' file format, laboratory, donor organism, target and treatment.
+#' 
+#' By default, the query can be made on an exact match term. This behaviour can 
+#' be modified by setting the \code{fixed} argument at \code{TRUE}
+#' 
+#' @param selection criteria.
+#'
+#' @return a \code{list} of two \code{data.frame}s containing data about ENCODE 
+#' experiments and datasets
+#'
+#' @examples
+#' matrices = ENCODEdb:export_ENCODEDB_matrix(database_filename)
+#' encode_exp = ENCODEdb::matrices[[1]]
+#' encode_ds = ENCODEdb::matrices[[2]]
+#' res = ENCODEdb::query(fileFormat = "GTF", biosample = "mcf-7", fixed = F)
+query <- function(acces = NULL, assayName = NULL, biosample = NULL, 
+                  dataset_access = NULL, fileAccess = NULL, fileFormat = NULL, 
+                  labo = NULL, organism = NULL, target = NULL, treatment = NULL,
+                  fixed = TRUE) {
+  
+  s1 = encode_exp
+  s2 = encode_ds
+  
+  if(fixed) {
+    
+    if(!is.null(acces)) {
+      s1 <- subset(s1, s1$accession == acces)
+      s2 <- subset(s2, s2$accession == acces)
+    }
+    
+    if(!is.null(assayName)) {
+      s1 <- subset(s1, s1$assay == assayName)
+    }
+    
+    if(!is.null(biosample)) {
+      s1 <- subset(s1, s1$biosample_name == biosample)
+    }
+    
+    if(!is.null(dataset_access)) {
+      s1 <- subset(s1, s1$dataset_accession == dataset_access)
+      s2 <- subset(s2, s2$accession == dataset_access)
+    }
+    
+    if(!is.null(fileAccess)) {
+      s1 <- subset(s1, s1$file_accession == fileAccess)
+      s2 <- subset(s2, s2$file_accession == fileAccess)
+    }
+    
+    if(!is.null(fileFormat)) {
+      s1 <- subset(s1, s1$file_format == fileFormat)
+      s2 <- subset(s2, s2$file_format == fileFormat)
+    }
+    
+    if(!is.null(labo)) {
+      s1 <- subset(s1, s1$lab == labo)
+      s2 <- subset(s2, s2$lab == labo)
+    }
+    
+    if(!is.null(organism)) {
+      s1 <- subset(s1, s1$organism == organism)
+    }
+    
+    if(!is.null(target)) {
+      s1 <- subset(s1, s1$target == target)
+    }
+    
+    if(!is.null(treatment)) {
+      s1 <- subset(s1, s1$treatment == treatment)
+    }
+  }
+  else
+  {
+    # retirer ignorer les espaces, les tirets et la casse
+    # m cf 7 = MCf7 = mcf-7 = MCF-7 ... etc
+    
+    if(!is.null(acces)) {
+      query.transfo = query_transform(access)
+      select.entries = grepl(x = s1$accession, pattern = query.transfo, 
+                             ignore.case = T, perl = T)
+      s1 = s1[select.entries,]
+      
+      select.entries = grepl(x = s2$accession, pattern = query.transfo, 
+                             ignore.case = T, perl = T)
+      s2 = s2[select.entries,]
+    }
+    
+    if(!is.null(assayName)) {
+      query.transfo = query_transform(assayName)
+      select.entries = grepl(x = s1$assay, pattern = query.transfo, 
+                             ignore.case = T, perl = T)
+      s1 = s1[select.entries,]
+    }
+    
+    if(!is.null(biosample)) {
+      query.transfo = query_transform(biosample)
+      select.entries = grepl(x = s1$biosample_name, pattern = query.transfo, 
+                             ignore.case = T, perl = T)
+      s1 = s1[select.entries,]
+    }
+    
+    if(!is.null(dataset_access)) {
+      query.transfo = query_transform(dataset_access)
+      select.entries = grepl(x = s1$dataset_accession, pattern = query.transfo, 
+                             ignore.case = T, perl = T)
+      s1 = s1[select.entries,]
+      
+      select.entries = grepl(x = s2$accession, pattern = query.transfo, 
+                             ignore.case = T, perl = T)
+      s2 = s2[select.entries,]
+    }
+    
+    if(!is.null(fileAccess)) {
+      query.transfo = query_transform(fileAccess)
+      select.entries = grepl(x = s1$file_accession, pattern = query.transfo, 
+                             ignore.case = T, perl = T)
+      s1 = s1[select.entries,]
+      
+      select.entries = grepl(x = s2$file_accession, pattern = query.transfo, 
+                             ignore.case = T, perl = T)
+      s2 = s2[select.entries,]
+    }
+    
+    if(!is.null(fileFormat)) {
+      query.transfo = query_transform(fileFormat)
+      select.entries = grepl(x = s1$file_format, pattern = query.transfo, 
+                             ignore.case = T, perl = T)
+      s1 = s1[select.entries,]
+      
+      select.entries = grepl(x = s2$file_format, pattern = query.transfo, 
+                             ignore.case = T, perl = T)
+      s2 = s2[select.entries,]
+    }
+    
+    if(!is.null(labo)) {
+      query.transfo = query_transform(labo)
+      select.entries = grepl(x = s1$lab, pattern = query.transfo, 
+                             ignore.case = T, perl = T)
+      s1 = s1[select.entries,]
+      
+      select.entries = grepl(x = s2$lab, pattern = query.transfo, 
+                             ignore.case = T, perl = T)
+      s2 = s2[select.entries,]
+    }
+    
+    if(!is.null(organism)) {
+      query.transfo = query_transform(organism)
+      select.entries = grepl(x = s1$organism, pattern = query.transfo, 
+                             ignore.case = T, perl = T)
+      s1 = s1[select.entries,]
+    }
+    
+    if(!is.null(target)) {
+      query.transfo = query_transform(target)
+      select.entries = grepl(x = s1$target, pattern = query.transfo, 
+                             ignore.case = T, perl = T)
+      s1 = s1[select.entries,]
+    }
+    
+    if(!is.null(treatment)) {
+      query.transfo = query_transform(treatment)
+      select.entries = grepl(x = s1$treatment, pattern = query.transfo, 
+                             ignore.case = T, perl = T)
+      s1 = s1[select.entries,]
+    }
+    
+  }
+  
+  
+  
+  if((nrow(s1) + nrow(s2)) == 0) {
+    warning("No result found. You can try the <search>", call. = F)
+    list()
+  }
+  else
+  {
+    if(nrow(s1) == nrow(encode_exp) || nrow(s2) == nrow(encode_ds)) {
+      warning("Please provide at least one valid criteria", call. = F)
+      list()
+    }
+    else
+    {
+      list(experiment = s1, dataset = s2)
+    }
+  }
+  
+  
+}
+
+query_transform <- function(my.term) {
+  my.term = gsub(my.term, pattern = " ", replacement = "", fixed = T)
+  my.term = gsub(my.term, pattern = "-", replacement = "", fixed = T)
+  my.term = gsub(my.term, pattern = ",", replacement = "", fixed = T)
+  my.term = strsplit(my.term, split = "")[[1]]
+  my.term.4.grep = paste0("^",paste(my.term, collapse = "[ ,-]?"))
+  
+  my.term.4.grep
+}
+
+
+#' Download files from the Internet.
+#'
+#' After processing to a basic search with the \code{search} function or a 
+#' precise search thanks to the \code{query} function, you can proceed to the
+#' downloading of all the corresponding files.
+#'
+#' This function can be used to download a set of files by providing the results
+#' set, its origin (search or query), the file format and finally the destination
+#' directory.
+#'
+#' @param searchResult the results set
+#' @param resultOrigin name of the function used to generate the result set 
+#' (\code{search} or \code{query})
+#' @param format file format, default = all
+#' @param dir the name of the directory where the downloaded file will be saved.
+#' Default = /tmp
+#'
+#'
+#' @examples
+#' res = ENCODEdb::search(searchTerm = "mcf7", limit = 2)
+downloadFile <- function(searchResult = NULL , resultOrigin = NULL, 
+                         format = "all", dir = "/tmp") {
+  if(is.null(searchResult) || is.null(resultOrigin)) {
+    warning("You have to provide both results set and its origin to use the 
+            download function", call. = F)
+  }
+  else
+  {
+    if(resultOrigin %in% c("search", "query"))
+    {
+      encode_root = "https://www.encodeproject.org"
+      if(file.access(dir, mode = 2) == 0) {
+        filesId = getFileId(searchResult = searchResult, 
+                            resultOrigin = resultOrigin, format = format)
+        
+        temp = subset(encode_exp, encode_exp$file_accession %in% filesId)
+        temp2 = subset(encode_ds, encode_ds$file_accession %in% filesId)
+        urls = c(as.character(temp$href), as.character(temp2$href))
+        urls
+        for (url in urls) {
+          fileName = strsplit(x = url, split = "@@download",fixed = T)[[1]][2]
+          ret = download.file(url = paste0(encode_root,url), quiet = T,
+                              destfile = paste0(dir,fileName), method = "wget" )
+        }
+        
+      }
+      else
+      {
+        warning(paste0("Can't write in ", dir), call. = F)
+      }
+    }
+    # origin farfelue 
+    else
+    {
+      warning("You have to provide a valid results set origin to use the 
+            download function : search or query", call. = F)
+    }
+  }
+}
+
+getFileId <- function(searchResult, resultOrigin, format = "all") {
+  
+  if(resultOrigin == "search") {
+    if(class(searchResult) == "data.frame")
+    {
+      d = getFileDetails(searchResult)
+    }
+    else
+    {
+      warning("Unexpected format for a result set coming from our search 
+              function", call. = F)
+    }
+    
+  }
+  else
+  {
+    if(class(searchResult) == "list" && length(searchResult) == 2)
+    {
+      d = searchResult
+    }
+    else
+    {
+      warning("Unexpected format for a result set coming from our query 
+              function", call. = F)
+    }
+  }
+  
+  
+  r = c()
+  formats = c(levels(d$experiment$file_format),levels(d$dataset$file_format))
+  if(format != "all") {
+    if(!(format %in% formats)) {
+      warning("Unknown file format", call. = F)
+    }
+    else
+    {
+      avail_format =  unique(c(as.character(d$experiment$file_format),
+                               as.character(d$dataset$file_format)))
+      if(!(format %in% avail_format)) {
+        warning("This file format is not available in your dataset", call. = F)
+      }
+      else
+      {
+        temp = subset(d$experiment, d$experiment$file_format == format)
+        temp2 = subset(d$dataset, d$dataset$file_format == format)
+        r = c(as.character(temp$file_accession), 
+              as.character(temp2$file_accession))
+      }
+    }
+  }
+  else {
+    r = c(as.character(d$experiment$file_accession), 
+          as.character(d$dataset$file_accession))
+  }
+  
+  r
+}
+
+getFileDetails <- function(searchResult) {
+  details = list(
+    experiment = getExperimentDetails(searchResult),
+    dataset = getDatasetDetails(searchResult)
+  )
+  details
+}
+
+getExperimentDetails <- function(searchResult) {
+  exp = searchResult$accession
+  subset(encode_exp,encode_exp$accession %in% exp)
+}
+
+getDatasetDetails <- function(searchResult) {
+  ds = searchResult$accession
+  subset(encode_ds,encode_ds$accession %in% ds)
 }
