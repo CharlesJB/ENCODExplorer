8dbbbbcce38b9e0c42b5934edc164d038bfaff33
Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2015-03-25 14:40:48 -0400

    Updated the data documentation
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- R/ENCODEdb.R
+++ R/ENCODEdb.R
@@ -4,16 +4,11 @@
 #' @docType package
 NULL
 
-#' List of selected tables from ENCODE rest api.
+#' Metadata from the ENCODE database in a R object.
 #' 
-#' The tables were generated with the \code{prepare_ENCODEdb} function.
-#'
-#' @seealso \code{\link{get_encode_types}} to get a list of possible types. Note
-#'   that some of the types are empty tables that are not included in the
-#'   database created with \code{\link{prepare_ENCODEdb}} function.
 #' @docType data
 #' @keywords datasets
-#' @name tables
-#' @usage data(tables)
+#' @name matrices
+#' @usage data(matrices)
 #' @format A list of data frames
 NULL
