113de92cef7042dc3f88b12de2da5d2a9ca623b6
c.beauparlant <c.beauparlant@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-03-11 20:04:38 +0000

    Fixed conflicts
    Signed-off-by: Charles Joly Beauparlant <Charles.Joly-Beauparlant@crchul.ulaval.ca>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ENCODExplorer@114653 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -218,34 +218,3 @@ export_ENCODEdb_matrix <- function(database_filename) {
   
   list(experiment = encode_exp, dataset = encode_ds)
 }
-
-
-
-#' A list of known tables from ENCODE database.
-#'
-#' The type (table) names are extracted from the schema list from ENCODE-DCC
-#' github repository:
-#'   https://github.com/ENCODE-DCC/encoded/tree/master/src/encoded/schemas
-#'
-#' The data is extracted using the github api:
-#'   https://developer.github.com/guides/getting-started/
-#'
-#' @return a vector of \code{character} with the names of the known tables in
-#'   the ENCODE database.
-#'
-#' @import tools
-#' @import Hmisc
-get_encode_types <- function() {
-  clean_name <- function(x) {
-	s <- strsplit(x, "_")[[1]]
-    paste(lapply(s, capitalize), collapse = "")
-  }
-  encode_api_url <- "https://api.github.com/repos"
-  encoded_repo <- "encode-dcc/encoded"
-  schemas <- "src/encoded/schemas"
-  url <- paste(encode_api_url, encoded_repo, "contents", schemas, sep = "/")
-  name <- jsonlite::fromJSON(url)
-  name <- name[grepl(".json", name[["name"]]),]
-  name <- tools::file_path_sans_ext(name[["name"]])
-  vapply(name, clean_name, character(1))
-}
