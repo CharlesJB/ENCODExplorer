5f3960be58c7b96f62c64f9ac0b5029b6766d870
Louis Gendron <louisg.212@gmail.com>
2016-05-11 13:02:03 -0400

    All these unitTest have been merged into a single script inst/test_clean_column.R

--- inst/unitTests/test_cleanTableR/clean_column_list_numeric.R
+++
@@ -1,9 +0,0 @@
-test.column_list_numeric <- function(){
-  #a <- c(1,2,3)
-  #b <- c(4,5)
-  #c <- c(NULL,6,7)
-  #my_list = list (a,b,c)
-  #Comment gerer si la taille des vecteurs numeriques ne sont pas pareils
-  #df<-data.frame(title=my_list)
-  TRUE
-}
--- inst/unitTests/test_cleanTableR/clean_column_numeric.R
+++
@@ -1,8 +0,0 @@
-test.column_numeric <- function(){
-  library(RUnit)
-  input <- c(11,22,33)
-  exp <- input
-  df <- data.frame(title=input,stringsAsFactors = FALSE)
-  obs <- ENCODExplorer:::clean_column("title",df)
-  checkIdentical(obs,exp)
-}
