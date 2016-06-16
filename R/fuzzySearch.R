3801a1329f8fa0a5556b4c248d611b737530ac16
Louis Gendron <louisg.212@gmail.com>
2016-06-16 09:20:06 -0400

    New documentation Changed default inputIsList by multipleTerm New way to filter and inform the user about invalid filter

--- R/fuzzySearch.R
+++ R/fuzzySearch.R
@@ -1,12 +1,33 @@
-# filterVector is a vector character that indicate not to apply the search on specific
-# column. Value of 1 is the column is selected and 0 if its not.
-# accesion - dataset_type - lab - title - file_type - platform - project - control
-# biosample_type - replicate - organism
+
+#' Fuzzysearch is a searching function for a string or a list of string 
+#' within the encode_df \code{data.table}
+#' @param searchTerm The keyword or a \code{list} of keyword to search. 
+#' @param database A \code{data.table}
+#' @param filterVector 
+#' @param multipleTerm A boolean that indicate if the searchTerm is a list or
+#' even multiple searchTerm separete by a comma in a single string.
+#' @param fixed Boolean use to apply a strict search for the searchTerm within
+#' the database
+#' 
+#' @return A \code{data.table} corresponding the every row of the database that
+#' contain at least of one the searchTerm.
 #' @importFrom dplyr filter
 
-fuzzySearch <- function(searchTerm=NULL, database=NULL, inputIsList=FALSE,
-                        filterVector=NULL, filter=FALSE, fixed = FALSE){
-    stopifnot(is.character(searchTerm))
+fuzzySearch <- function(searchTerm=NULL, database=NULL,filterVector=NULL,
+                        multipleTerm=FALSE, fixed = FALSE){
+    filter <- FALSE
+    if(!(is.list(searchTerm)|is.character(searchTerm)|is.null(searchTerm))){
+        print("Invalid searchTerm input")
+        return()
+    }
+    #Converting the list type of input into a single string
+    if(is.list(searchTerm)){
+        multipleTerm <- TRUE
+        searchTerm <- paste(searchTerm, collapse = ",")
+    }
+    
+    
+    #Loading the default data.tables from ENCODExplorer package
     if(is.null(database)){
         load(file=system.file("../data/encode_df.rda", package="ENCODExplorer"))
     }else if(is.data.table(database)){
@@ -23,18 +44,16 @@ fuzzySearch <- function(searchTerm=NULL, database=NULL, inputIsList=FALSE,
     if(!is.null(filterVector)){
         filter <- TRUE
         match <- filterVector %in% filterNames
-        
-        for(i in 1:length(filterVector)){
-            if(!match[i]){
-                print(paste(filterVector[i],"is not a valid filter and it will be remove", sep = " "))
-            }
+        if(sum(as.logical(match)) != length(filterVector)){
+            print(paste("These filter were invalid and will not be considered",
+                    filterVector[!match],sep=", "))
+            filterVector <- filterVector[match]
         }
-        filterVector <- filterVector[match]
     }
     
     
     # Converting the list of entry in a valid format
-    if(inputIsList){
+    if(multipleTerm){
         searchTerm <- gsub(pattern="\n", replacement="", x=searchTerm)
         searchTerm <- gsub(pattern=" , ", replacement="|", x=searchTerm)
         searchTerm <- gsub(pattern=", ", replacement="|", x=searchTerm)
@@ -45,7 +64,7 @@ fuzzySearch <- function(searchTerm=NULL, database=NULL, inputIsList=FALSE,
     search_list <- vector(mode="list", ncol(encode_df))
     zeroVector <- rep(0,nrow(encode_df))
     
-    # Looking for the searchTerm in every column
+    # Looking for the searchTerm in every column 
     for (i in 1:length(search_list)) {
         if (filter){
             if(names(encode_df)[[i]] %in% filterVector) {
