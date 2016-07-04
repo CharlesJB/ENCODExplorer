224f949d21df95740c34cc0258df8e924dcd8ece
Louis Gendron <louisg.212@gmail.com>
2016-07-04 11:10:34 -0400

    Organized the conditional panel with the design options with fluidRow()

--- R/ui.R
+++ R/ui.R
@@ -1,12 +1,13 @@
 library(shiny)
 library(shinythemes)
+library(DT)
 ui <- fluidPage(theme = shinytheme("cerulean"),
         navbarPage("ENCODExplorer",
 #////----------------------------fuzzySearch-------------------------------////
         tabPanel("Search",sidebarLayout(
             sidebarPanel(
                     actionButton("searchAction", "Search"),
-                    fileInput("df", "Import your database (optional)"),
+                    #fileInput("df", "Import your database (optional)"),
                     radioButtons("typeInput","Select your type of search",
                         choices = list("Single element"=1, "Mutiple element"=2),
                         selected=1),
@@ -17,13 +18,16 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                                     "Target"=14,"Lab"=3, "Title"=4, "File Type"=6, "Platform"=16,
                                     "Project"=7, "Type"=8, "Control"=9, "Biosample Type"=10,
                                     "Biosample Name"=16,"Replicate"=11, "Organism"=12,
-                                    "Lab"=3,"Assay"=15,"File format"=5))),
+                                    "Lab"=3,"Assay"=15,"File format"=5)),
+                    width = 3),
             mainPanel(
                 conditionalPanel(
                     condition = "input.searchAction > 0",
-                    fluidRow(column(6, checkboxInput("splitFromSearch",
+                    fluidRow(column(3,actionButton("designFromSearch",
+                                                   "Create a design")),
+                            column(5, checkboxInput("splitFromSearch",
                                 "Split the result per experiment", value=FALSE)),
-                            column(6, radioButtons("formatFromSearch", "Design format",
+                            column(4, radioButtons("formatFromSearch", "Design format",
                                 choices=list("long"=1, "wide"=2 ),inline=TRUE,
                                 selected=1))),
                     fluidRow(column(3, selectInput("fileFromSearch", "File Format",
@@ -42,18 +46,19 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                             column(3,numericInput("ctrlFromSearch",
                                 "Control ID",
                                 value=2))),
-                    fluidRow(column(6,actionButton("downloadFromSearch", "Download")),
-                        column(6,actionButton("designFromSearch", "Create a design")))
-                ),
+                    fluidRow(column(3, actionButton("downloadFromSearch", "Download")),
+                             column(9, verbatimTextOutput("consoleSearch"))),
+                    textOutput("fileSizeFuzzy")),
+                
                 
                 conditionalPanel(
                     condition="!output.designVis",
                     tags$head(tags$style(".table .alignCenter {text-align:center;}")),
-                    dataTableOutput("searchResult")),
+                    DT::dataTableOutput("searchResult")),
                 
                 conditionalPanel(
                     condition="output.designVis & !input.splitFromSearch",
-                    dataTableOutput("designResultSearch")),
+                    DT::dataTableOutput("designResultSearch")),
 
                 conditionalPanel(
                     condition="output.designVis & input.splitFromSearch",
@@ -82,36 +87,40 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
                 textInput("project", "Project"),
                 selectInput("fileStatus", "FileStatus", choices = list(
                             "released"=1, "revoked"=2, "All"=3),selected=1),
-                textInput("status", "Status", value="released")),
+                textInput("status", "Status", value="released"),
+                width=3),
             
             mainPanel(
                 conditionalPanel(
                     condition = "input.searchAdvanced > 0",
-                    actionButton("designFromQuery", "Create a design"),
-                    checkboxInput("splitFromQuery",
-                                  "Split the result per experiment", value=FALSE),
-                    radioButtons("formatFromQuery", "Design format",
-                                 choices =list("long"=1, "wide"=2 ), selected=1),
-                    selectInput("fileFromQuery", "File Format", choices = list(
-                                "bam"=1, "fastq"=2, "fasta"=3, "sam"=4, "bed"=5,
-                                "bigBed"=6,"bigWig"=7)),
-                    selectInput("datatypeFromQuery","Data Type", choices =list(
-                                "experiments"=1,"ucsc browser composite"=2,
+                    fluidRow(column(3, actionButton("designFromQuery",
+                                                   "Create a design")),
+                            column(5, checkboxInput("splitFromQuery",
+                                  "Split the result per experiment", value=FALSE)),
+                            column(4, radioButtons("formatFromQuery", "Design format",
+                                 choices =list("long"=1, "wide"=2 ), selected=1))),
+                    fluidRow(column(3, selectInput("fileFromQuery", "File Format",
+                                choices = list("bam"=1, "fastq"=2, "fasta"=3,
+                                        "sam"=4, "bed"=5,"bigBed"=6,"bigWig"=7))),
+                            column(3, selectInput("datatypeFromQuery","Data Type",
+                                choices =list("experiments"=1,"ucsc browser composite"=2,
                                 "annotations"=3,"matched sets"=4, "projects"=5,
-                                "reference epigenomes"=6,"references"=7)),
-                    textInput("repFromQuery",
-                              "Numeric ID assigned to replicate file", value="1"),
-                    textInput("ctrlFromQuery",
-                              "Numeric ID assigned to control file", value="2")),
+                                "reference epigenomes"=6,"references"=7))),
+                            column(3, numericInput("repFromQuery","Replicate ID",
+                                                   value=1)),
+                            column(3, numericInput("ctrlFromQuery",
+                              "Control ID", value=2))),
+                    fluidRow(column(3,actionButton("downloadFromQuery", "Download")),
+                            column(9,verbatimTextOutput("consoleQuery")))),
                 
                     conditionalPanel(
                         condition="!output.designVis",
                         tags$head(tags$style(".table .alignCenter {text-align:center;}")),
-                        dataTableOutput("advancedResult")),
+                        DT::dataTableOutput("advancedResult")),
                 
                     conditionalPanel(
                         condition="output.designVis & !input.splitFromQuery",
-                        dataTableOutput("designAdvanced")),
+                        DT::dataTableOutput("designAdvanced")),
                 
                     conditionalPanel(
                         condition="output.designVis & input.splitFromQuery",
