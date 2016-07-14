7ebde2296614c3d3610af360ef0cf8c17850a991
Louis Gendron <louisg.212@gmail.com>
2016-07-14 12:38:06 -0400

    Trying to fix browser for linux

--- R/shiny_encode.R
+++ R/shiny_encode.R
@@ -1,9 +1,12 @@
 
-shinyEncode <- function(){
+shinyEncode <- function(inbrowser=F){
   browser_p <- "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
   source(file = system.file("inst/shiny/ui.R", package = "ENCODExplorer"))
   source(file = system.file("shiny/server.R", package = "ENCODExplorer"))
-  options(browser = getOption("browser"))
-  runApp(appDir = shinyApp(ui,server),host="127.0.0.1",port=8888, launch.browser = T)
-  
+  if(inbrowser) {
+    options(browser = getOption("browser"))
+    runApp(appDir = shinyApp(ui,server),host="127.0.0.1",port=8888, launch.browser = T)
+  } else {
+    runApp(appDir = shinyApp(ui,server),host="127.0.0.1",port=8888)
+  }
 }
