c303b6660d567c526ef02497625ce460616006ef
Louis Gendron <louisg.212@gmail.com>
2016-07-14 14:50:47 -0400

    test for linux version

--- R/shiny_encode.R
+++ R/shiny_encode.R
@@ -1,6 +1,6 @@
-
+# 
 shinyEncode <- function(){
   source(file = system.file("inst/shiny/ui.R", package = "ENCODExplorer"))
   source(file = system.file("shiny/server.R", package = "ENCODExplorer"))
-  runApp(appDir = shinyApp(ui,server),port=8888)
+    runApp(appDir = shinyApp(ui,server))
 }
