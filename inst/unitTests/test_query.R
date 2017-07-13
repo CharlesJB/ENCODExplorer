0abf123dbc2a84f2c15c7604d6ca4c25c27aa0d1
Eric Fournier <eric.fournier@fsaa.ulaval.ca>
2017-07-13 14:52:14 -0400

    Fixes #31. Implements #8.
    Add function queryEncodeGeneric which provides support for regular expressions and multiple values per search term.
    Also properly use the passed-in df argument when the built-in encode_df should not be used.

--- inst/unitTests/test_query.R
+++ inst/unitTests/test_query.R
@@ -14,18 +14,18 @@ test.fixed_option <- function() {
 
 test.query_with_target <- function() {
   data("encode_df")
-  res_regexp = queryEncode(df=encode_df, target = "rabbit iGG", fixed=F, quiet=T)
+  res_regexp = queryEncode(df=encode_df, target = "rabbit iGG", fixed=T, quiet=T, fuzzy=T)
   checkEquals(sum(!grepl("experiments", x=res_regexp$dataset_type)), 0,
               msg = "2.query with a target filter shouldn't return any dataset results" )
   checkTrue(sum(grepl("experiments", x=res_regexp$dataset_type)) > 0,
-            msg = "queryEncode with approximative target name and fixed = F should return at least one result" )
+            msg = "queryEncode with approximative target name and fuzzy = T should return at least one result" )
 }
 
 
 test.combined_query <- function() {
   data("encode_df")
   res_combo = queryEncode(df=encode_df, biosample_name = "A549", assay = "chipseq",
-                          file_format = "bigwig", fixed = F, quiet=T)
+                          file_format = "bigwig", fixed = T, quiet=T, fuzzy=T)
   checkTrue("ENCFF000VPN" %in% as.character(res_combo$file_accession),
             msg = "this combined query should return a results set containing the id ENCFF000VPN")
 }
@@ -42,9 +42,39 @@ test.combined_query <- function() {
 
 test.query_revoked_file <- function() {
   data("encode_df")
-  res_revok = queryEncode(assay = "chipseq", biosample_name = "mcf7", fixed = F,
-                          file_status = "revoked", df=encode_df)
+  res_revok = queryEncode(assay = "chipseq", biosample_name = "mcf7", fixed = T,
+                          file_status = "revoked", df=encode_df, fuzzy=T)
 
   checkTrue("ENCFF000ZKM" %in% as.character(res_revok$file_accession),
             msg = "this combined query should return a result set containg ENCFF000ZKM")
+}
+
+test.query_two_terms <- function() {
+  data("encode_df")
+  assay1 = "small RNA-seq"
+  assay2 = "polyA mRNA RNA-seq"
+  res_two_terms = queryEncode(assay = c(assay1, assay2), biosample_name = "HeLa-S3", fixed = T)
+
+  checkTrue(assay1 %in% res_two_terms$assay && assay2 %in% res_two_terms$assay,
+            msg = paste0("this combined query should return a result set containg both ", assay1, " and ", assay2))
+}
+
+test.query_fuzzy_prefix <- function() {
+  data("encode_df")
+  assay1 = "small RNA-seq"
+  assay2 = "polyA mRNA RNA-seq"
+  res_fuzzy = queryEncode(assay = "RNA-seq", biosample_name = "HeLa-S3", fixed = T, fuzzy = T)
+
+  checkTrue(assay1 %in% res_fuzzy$assay && assay2 %in% res_fuzzy$assay,
+            msg = "this fuzzy query should return all assays which have RNA-seq as a substring")
+}
+
+test.query_regex <- function() {
+  data("encode_df")
+  assay1 = "small RNA-seq"
+  assay2 = "polyA mRNA RNA-seq"
+  res_two_terms = queryEncode(assay = ".*RNA-seq", biosample_name = "HeLa-S3", fixed = F)
+
+  checkTrue(assay1 %in% res_two_terms$assay && assay2 %in% res_two_terms$assay,
+            msg = "this regex query should return all assays which have RNA-seq as a substring")
 }
