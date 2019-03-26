93e318f959ae87ad47ebdb615884ca2421700a92
Eric Fournier <eric.fournier@fsaa.ulaval.ca>
2019-03-26 14:14:17 -0400

    Add documentation for encode_df_ext objects.

--- R/ENCODExplorer.R
+++ R/ENCODExplorer.R
@@ -19,5 +19,45 @@ NULL
 #' @return A data table
 NULL
 
+#' Extended metadata (1) from the ENCODE database in a R object.
+#' \code{get_encode_df_full} should be used instead of accessing 
+#' this object directly.
+#'
+#' @seealso \code{\link{get_encode_df_full}}, \code{\link{export_ENCODEdb_matrix_lite}}.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name encode_df_ext_1
+#' @usage data(encode_df_ext_1)
+#' @format A data table
+#' @return A data table
+NULL
 
+#' Extended metadata (2) from the ENCODE database in a R object.
+#' \code{get_encode_df_full} should be used instead of accessing 
+#' this object directly.
+#'
+#' @seealso \code{\link{get_encode_df_full}}, \code{\link{export_ENCODEdb_matrix_lite}}.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name encode_df_ext_2
+#' @usage data(encode_df_ext_2)
+#' @format A data table
+#' @return A data table
+NULL
+
+#' Extended metadata (3) from the ENCODE database in a R object.
+#' \code{get_encode_df_full} should be used instead of accessing 
+#' this object directly.
+#'
+#' @seealso \code{\link{get_encode_df_full}}, \code{\link{export_ENCODEdb_matrix_lite}}.
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name encode_df_ext_3
+#' @usage data(encode_df_ext_3)
+#' @format A data table
+#' @return A data table
+NULL
 
